bab367602a1a31ba1c677488781899a8874eeb95
maugelves <mgelves@gmail.com>
2017-08-03 14:02:51 +0200

    Removed acf-plugin configurations. The menu must be disabled from theme configurations, not from the plugin.

--- inc/acfs/acf-plugin.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// 3. Hide ACF field group menu item
-add_filter( 'acf/settings/show_admin', '__return_false' );
---
+++ inc/classes/obra-model.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace mgtc\models;
+
+
+class Obra {
+
+
+	/* =================================
+     * PRIVATE VARIABLES
+     * ================================*/
+	private $title  = '';
+
+
+
+
+	private $allMyCoupons           = null;
+	private $allMyAvailableCoupons  = null;
+	private $birthdate              = '';
+	private $have_insurance         = false;
+	private $id                     = '';
+	private $email                  = '';
+	private $firstname              = '';
+	private $know_us_by             = '';
+	private $lastname               = '';
+	private $mobile                 = '';
+	private $myDiscounts            = null;
+	private $password               = '';
+	private $recurrent_pay_code             = '';
+	private $recurrent_pay_code_expiration = '';
+
+
+}
